243ff2203b4fa8f9d52c90610d10510b7267b8e2
Waken Norman <wakennorman@outlook.com>
2026-07-10 03:11:41 +0800

    chore: 提交未暂存改动（欢迎界面默认显示+静态资源配置+文档）

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,11 @@
 export default {
   base: "./",
-  publicDir: false,
+  publicDir: "src/images",
   server: {
     host: "127.0.0.1",
     port: 5173,
+    // 允许访问 src/ 下的静态文件
+    fs: { allow: ["."] },
   },
   preview: {
     host: "127.0.0.1",
